5cd2ac2107542e2b8060a58bf4357fbd63e79308
kelsonic <kgadams93@gmail.com>
2017-01-08 21:34:11 -0700

    Set i18n messages for component generator default to false

--- internals/generators/component/index.js
+++ internals/generators/component/index.js
@@ -27,7 +27,7 @@ module.exports = {
   }, {
     type: 'confirm',
     name: 'wantMessages',
-    default: true,
+    default: false,
     message: 'Do you want i18n messages (i.e. will this component use text)?',
   }],
   actions: (data) => {
